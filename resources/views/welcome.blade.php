14ad7c4c6c1fc18fc22e6760826b61d9743d9b1a
euclimauro <euclimauro@hotmail.com>
2018-06-28 02:29:36 +0200

    updated .env file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Evaclerm Deployment Testing
                 </div>
 
                 <div class="links">
